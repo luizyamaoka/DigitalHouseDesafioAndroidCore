78e6afe1064eeb4aeae0949b8b9ed2f1264a4daf
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2020-11-07 18:29:56 -0300

    add dish screen

--- app/src/main/java/br/com/digitalhouse/desafioandroidcore/RestaurantDishActivity.kt
+++ app/src/main/java/br/com/digitalhouse/desafioandroidcore/RestaurantDishActivity.kt
@@ -2,10 +2,25 @@ package br.com.digitalhouse.desafioandroidcore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.digitalhouse.desafioandroidcore.domain.Dish
+import kotlinx.android.synthetic.main.activity_restaurant_dish.*
 
 class RestaurantDishActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restaurant_dish)
+        setSupportActionBar(toolbar)
+
+        var dish = intent.getSerializableExtra("dish") as Dish
+
+        ivDish.setImageResource(dish.image)
+        tvDishName.text = dish.name
+        tvDishDescription.text = dish.description
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
--- app/src/main/java/br/com/digitalhouse/desafioandroidcore/RestaurantMenuActivity.kt
+++ app/src/main/java/br/com/digitalhouse/desafioandroidcore/RestaurantMenuActivity.kt
@@ -17,7 +17,6 @@ class RestaurantMenuActivity : AppCompatActivity() {
         tvRestaurantName.text = restaurant.name
         ivRestaurant.setImageResource(restaurant.image)
 
-//        val dish = Dish(1, "Salada com molho Gengibre", R.drawable.image4, "Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusant doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritalis.")
         rvDishes.adapter = RestaurantMenuAdapter(this, restaurant.dishes)
         rvDishes.layoutManager = GridLayoutManager(this, 2)
     }
