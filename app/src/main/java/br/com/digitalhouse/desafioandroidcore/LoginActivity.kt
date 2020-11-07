d07e18af73ed74954da2b95d2aac1a26396faa01
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2020-11-07 15:17:23 -0300

    add login activity

--- app/src/main/java/br/com/digitalhouse/desafioandroidcore/LoginActivity.kt
+++ app/src/main/java/br/com/digitalhouse/desafioandroidcore/LoginActivity.kt
@@ -2,10 +2,14 @@ package br.com.digitalhouse.desafioandroidcore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.digitalhouse.desafioandroidcore.helper.AsteriskPasswordTransformationMethod
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        inputPassword.transformationMethod = AsteriskPasswordTransformationMethod()
     }
 }
---
+++ app/src/main/java/br/com/digitalhouse/desafioandroidcore/helper/AsteriskPasswordTransformationMethod.kt
@@ -0,0 +1,26 @@
+package br.com.digitalhouse.desafioandroidcore.helper
+
+import android.text.method.PasswordTransformationMethod
+import android.view.View
+
+
+class AsteriskPasswordTransformationMethod : PasswordTransformationMethod() {
+    override fun getTransformation(source: CharSequence?, view: View?): CharSequence {
+        return PasswordCharSequence(source!!)
+    }
+
+    private inner class PasswordCharSequence(private val mSource: CharSequence) : CharSequence {
+
+        override val length: Int
+            get() = mSource.length
+
+        override fun get(index: Int): Char {
+            return '*'
+        }
+
+        override fun subSequence(start: Int, end: Int): CharSequence {
+            return mSource.subSequence(start, end) // Return default
+        }
+
+    }
+}
