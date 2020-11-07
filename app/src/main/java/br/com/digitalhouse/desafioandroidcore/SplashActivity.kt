da5849cdd8543d320e9f302229a3715f68762f31
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2020-11-07 14:30:35 -0300

    add splash screen time delay

--- app/src/main/java/br/com/digitalhouse/desafioandroidcore/SplashActivity.kt
+++ app/src/main/java/br/com/digitalhouse/desafioandroidcore/SplashActivity.kt
@@ -1,11 +1,22 @@
 package br.com.digitalhouse.desafioandroidcore
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
+
+    private val splashScreenTime : Long = 3000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, splashScreenTime)
     }
 }
