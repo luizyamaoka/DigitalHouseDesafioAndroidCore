dcd07d0b60beef00883c5d2d7132dbd6d29835a8
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2020-11-07 18:46:27 -0300

    add intent from registration to restaurants list

--- app/src/main/java/br/com/digitalhouse/desafioandroidcore/RegistrationActivity.kt
+++ app/src/main/java/br/com/digitalhouse/desafioandroidcore/RegistrationActivity.kt
@@ -1,11 +1,13 @@
 package br.com.digitalhouse.desafioandroidcore
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.digitalhouse.desafioandroidcore.helper.AsteriskPasswordTransformationMethod
 import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
@@ -14,6 +16,11 @@ class RegistrationActivity : AppCompatActivity() {
         inputPassword.transformationMethod = AsteriskPasswordTransformationMethod()
         inputPasswordConfirmation.transformationMethod = AsteriskPasswordTransformationMethod()
 
+        btnRegister.setOnClickListener{
+            startActivity(
+                Intent(this, RestaurantsListActivity::class.java)
+            )
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
